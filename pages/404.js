ef1de4b873ed6526a1039fd388a644955272c891
Jerome Tolentino <tolent.jay@gmail.com>
2021-04-20 20:05:01 +0900

    added useEffect

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,16 @@
 import Link from 'next/link';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const NotFound = () => {
+  const router = useRouter(); // routes the use to another page
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/');
+    }, 3000);
+  }, []);
+
   return (
     <div className='not-found'>
       <h1>Oooooops....</h1>
