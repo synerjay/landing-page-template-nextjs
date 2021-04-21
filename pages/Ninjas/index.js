bd94660543b177dee11e4301f1dd92ea3a4aa144
Jerome Tolentino <tolent.jay@gmail.com>
2021-04-21 20:44:49 +0900

    yay

--- pages/Ninjas/index.js
+++ pages/Ninjas/index.js
@@ -1,5 +1,29 @@
-const Ninjas = () => {
-  return <div>This is a ninjas Page!</div>;
+import styles from '../../styles/Ninjas.module.css';
+
+// In next, we just getStaticProps method to get a Rest API and inject them as props in the component below
+
+export const getStaticProps = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+
+  return {
+    props: { ninjas: data },
+  };
+};
+
+const Ninjas = ({ ninjas }) => {
+  return (
+    <div>
+      <h1>All Ninjas</h1>
+      {ninjas.map((ninja) => (
+        <div key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ninja.name}</h3>
+          </a>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Ninjas;
--- pages/Ninjas/test.js
+++
@@ -1,5 +0,0 @@
-const Test = () => {
-  return <div>HELLO THIS A TEST!</div>;
-};
-
-export default Test;
