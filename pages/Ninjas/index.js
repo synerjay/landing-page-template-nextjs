0e47bcaa28c5845420483e7eb7a64c2568cca761
Jerome Tolentino <tolent.jay@gmail.com>
2021-04-23 20:01:58 +0900

    changed the rest api endpoint because jsonplaceholder is being friggin unreliable af

--- pages/Ninjas/index.js
+++ pages/Ninjas/index.js
@@ -2,24 +2,27 @@ import styles from '../../styles/Ninjas.module.css';
 import Link from 'next/link';
 
 export const getStaticProps = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
-  const data = await res.json();
+  const res = await fetch('https://reqres.in/api/users');
+  const api = await res.json();
 
   return {
-    props: { ninjas: data },
+    props: { ninjas: api },
   };
 };
 
 const Ninjas = ({ ninjas }) => {
+  console.log('THE API IS BELOW!');
   console.log(ninjas);
 
   return (
     <div>
       <h1>All Ninjas</h1>
-      {ninjas.map((ninja) => (
+      {ninjas.data.map((ninja) => (
         <Link href={'/ninjas/' + ninja.id} key={ninja.id}>
           <a className={styles.single}>
-            <h3>{ninja.name}</h3>
+            <h3>
+              {ninja.first_name} {ninja.last_name}
+            </h3>
           </a>
         </Link>
       ))}
