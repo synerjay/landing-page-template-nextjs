c1b4760bf009739aebe018c7eb172193115c19cb
Jerome Tolentino <tolent.jay@gmail.com>
2021-10-09 17:20:44 +0900

    added header for now

--- comps/Layout.js
+++ comps/Layout.js
@@ -1,14 +1,31 @@
 import Footer from './Footer';
 import Navbar from './Navbar';
+import Head from 'next/head';
 
-const Layout = ({ children }) => {
+const Layout = (props) => {
   return (
-    <div className='content'>
-      <Navbar />
-      {children}
-      <Footer />
+    <div>
+      <Head>
+        <title>{props.pageTitle}</title>
+        <meta charSet='UTF-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
+        <link
+          rel='stylesheet'
+          href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css'
+        />
+      </Head>
+      <div>{props.children}</div>
     </div>
   );
 };
 
 export default Layout;
+
+{
+  /* <div className='content'>
+<Navbar />
+{children}
+<Footer />
+</div> */
+}
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,44 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Footer from '../comps/Footer';
+import Header from '../comps/Header';
+import Layout from '../comps/Layout';
 import Navbar from '../comps/Navbar';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   // in React a component MUST have a single FRAGMENT. it cannot have TWO elements side by side each other
   return (
-    <>
-      <Head>
-        <title>Ninja List | Home</title>
-        <meta name='keywords' content='ninjas' />
-      </Head>
-      <div>
-        <h1 className={styles.title}>Homepage</h1>
-        <p className={styles.text}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam quia
-          perferendis qui quos rerum inventore earum. Harum velit dignissimos,
-          excepturi asperiores similique ipsam ipsa recusandae repudiandae sequi
-          doloribus, a inventore!
-        </p>
-        <p className={styles.text}>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci
-          nihil blanditiis iste obcaecati qui recusandae repudiandae alias
-          quibusdam sapiente deleniti facilis nemo tenetur repellat doloribus
-          natus, odio, sequi quae pariatur.
-        </p>
-        <Link href='ninjas'>
-          <a className={styles.btn}>See ninja listing</a>
-        </Link>
-      </div>
-    </>
+    <Layout pageTitle='Product Landing Page'>
+      <Header />
+      <h2>Hello</h2>
+    </Layout>
   );
+}
+
+{
+  /* <>
+<Head>
+  <title>Ninja List | Home</title>
+  <meta name='keywords' content='ninjas' />
+</Head>
+<div>
+  <h1 className={styles.title}>Homepage</h1>
+  <p className={styles.text}>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam quia
+    perferendis qui quos rerum inventore earum. Harum velit dignissimos,
+    excepturi asperiores similique ipsam ipsa recusandae repudiandae sequi
+    doloribus, a inventore!
+  </p>
+  <p className={styles.text}>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci
+    nihil blanditiis iste obcaecati qui recusandae repudiandae alias
+    quibusdam sapiente deleniti facilis nemo tenetur repellat doloribus
+    natus, odio, sequi quae pariatur.
+  </p>
+  <Link href='ninjas'>
+    <a className={styles.btn}>See ninja listing</a>
+  </Link>
+</div>
+</> */
 }
