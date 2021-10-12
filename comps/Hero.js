6fdc724b232aa25b195623b650d883ec400a35dc
Jerome Tolentino <tolent.jay@gmail.com>
2021-10-12 19:37:41 +0900

    added gradient animation

--- comps/Hero.js
+++ comps/Hero.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <section className='px-16 text-black'>
+    <section className='hero px-16 text-black'>
       <div className='container mx-auto flex px-5 py-24 items-center justify-center flex-col'>
         <div className='text-center lg:w-2/3 w-full'>
           <h1 className='title-font sm:text-4xl text-3xl mb-4 font-medium	 text-black font-mono'>
