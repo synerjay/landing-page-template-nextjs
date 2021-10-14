605d52b4ee3542961aadaafc87807b9203c1b930
Jerome Tolentino <tolent.jay@gmail.com>
2021-10-14 18:35:20 +0900

    added header

--- comps/Header.js
+++ comps/Header.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 const Header = () => {
-  // const [sticky, setSticky] = useState(false);
+  const [sticky, setSticky] = useState(false);
 
   // useEffect(() => {
   //   window.addEventListener('scroll', handleScroll);
