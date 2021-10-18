178b6b1ba30df58a973b469fd73c80d8c4aa010f
Jerome Tolentino <tolent.jay@gmail.com>
2021-10-18 15:17:48 +0900

    changed the padding on the company logos section

--- comps/Services.js
+++ comps/Services.js
@@ -19,12 +19,12 @@ const Services = () => {
     <section className='bg-white mt-20'>
       <div className='container px-24 pt-24 pb-10 mx-auto flex justify-center items-center flex-wrap'>
         <div className='flex flex-col justify-center items-center text-center w-full mb-2'>
-          <h2 className='text-xs text-black tracking-widest font-medium title-font mb-1'>
+          <h2 className='text-xs text-black tracking-widest font-medium title-font mb-3'>
             TRUSTED BY THESE LEADING COMPANIES AND STARTUPS
           </h2>
         </div>
-        <div className='p-4 md:w-2/3 '>
-          <div className='flex flex-row justify-around items-center rounded-lg h-full bg-gray-200 p-8'>
+        <div className='md:w-2/3 '>
+          <div className='flex flex-row justify-around items-center rounded-lg h-full bg-gray-200 p-3'>
             <img
               className='h-10'
               src='https://img.icons8.com/fluency/96/000000/stripe.png'
