95d882fe4e6d56e7b62889fe043259ba94111efb
Jerome Tolentino <tolent.jay@gmail.com>
2021-10-31 16:29:22 +0900

    Added testimonials second column

--- comps/Testimonials.js
+++ comps/Testimonials.js
@@ -39,7 +39,7 @@ const TestimonialCard = (props) => {
 };
 
 const Testimonials = () => {
-  const testimonials = [
+  const columnOne = [
     {
       id: 1,
       img: 'https://i.pravatar.cc/100?img=1',
@@ -58,22 +58,45 @@ const Testimonials = () => {
         'Lorem ipsum, dolor sit amet, consectetur adipisicing elit. Dolore quod necessitatibus, labore sapiente, est, dignissimos ullam error ipsam sint quam tempora vel.',
       link: '/',
     },
-    // {
-    //   id: 3,
-    //   img: './images/Group Members.png',
-    //   name: 'LOREM IPSUM',
-    //   title: 'CEO Workcation',
-    //   quote:
-    //     'Sed perspiciatis unde omnis natus error voluptatem accusantium doloremque laudantium totam rem aperiam eaque ipsa quae ab illo excepturi sint occaecati cupiditate architecto.',
-    //   link: '/',
-    // },
-    // {
-    //   id: 4,
-    //   img: './images/45.png',
-    //   title: 'LOREM IPSUM',
-    //   desc: 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
-    //   link: '/',
-    // },
+    {
+      id: 3,
+      img: 'https://i.pravatar.cc/100?img=3',
+      name: 'Tommie Ewart.',
+      title: 'CEO Workcation',
+      quote:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, obcaecati ullam excepturi dicta error deleniti sequi.',
+      link: '/',
+    },
+  ];
+
+  const columnTwo = [
+    {
+      id: 3,
+      img: 'https://i.pravatar.cc/100?img=3',
+      name: 'Tommie Ewart.',
+      title: 'CEO Workcation',
+      quote:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, obcaecati ullam excepturi dicta error deleniti sequi.',
+      link: '/',
+    },
+    {
+      id: 4,
+      img: 'https://i.pravatar.cc/100?img=4',
+      name: 'Charlie Howse.',
+      title: 'CEO Workcation',
+      quote:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus maxime, magni veritatis maiores vel nisi ab. Voluptatum ipsam, fuga eligendi sint quia ipsa cum corporis numquam nostrum illum facere. Odit.',
+      link: '/',
+    },
+    {
+      id: 5,
+      img: 'https://i.pravatar.cc/100?img=2',
+      name: 'Nevada Herbertson',
+      title: 'CEO Workcation',
+      quote:
+        'Lorem ipsum, dolor sit amet, consectetur adipisicing elit. Dolore quod necessitatibus, labore sapiente, est, dignissimos ullam error ipsam sint quam tempora vel.',
+      link: '/',
+    },
   ];
 
   return (
@@ -89,67 +112,12 @@ const Testimonials = () => {
             </h3>
           </div>
           <div className='-mx-3 md:flex items-start'>
-            {/* Column 1 */}
             <div className='px-3 md:w-1/3'>
-              {/* Card */}
-              <TestimonialCard testimonials={testimonials} />
+              <TestimonialCard testimonials={columnOne} />
             </div>
-            {/* End of Column 1 */}
-            {/* Column 2 */}
             <div className='px-3 md:w-1/3'>
-              <div className='w-full mx-auto rounded-lg TestiCard border border-gray-200 p-5 text-gray-200 font-light mb-6'>
-                <div className='w-full flex mb-4 items-center'>
-                  <div className='overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200'>
-                    <img src='https://i.pravatar.cc/100?img=3' alt='' />
-                  </div>
-                  <div className='flex-grow pl-3'>
-                    <h6 className='font-bold text-sm uppercase text-gray-200'>
-                      Tommie Ewart.
-                    </h6>
-                  </div>
-                </div>
-                <div className='w-full'>
-                  <p className='text-sm leading-tight'>
-                    <span className='text-lg leading-none italic font-bold text-gray-400 mr-1'>
-                      "
-                    </span>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Vitae, obcaecati ullam excepturi dicta error deleniti sequi.
-                    <span className='text-lg leading-none italic font-bold text-gray-400 ml-1'>
-                      "
-                    </span>
-                  </p>
-                </div>
-              </div>
-              <div className='w-full mx-auto rounded-lg TestiCard border border-gray-200 p-5 text-gray-200 font-light mb-6'>
-                <div className='w-full flex mb-4 items-center'>
-                  <div className='overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200'>
-                    <img src='https://i.pravatar.cc/100?img=4' alt='' />
-                  </div>
-                  <div className='flex-grow pl-3'>
-                    <h6 className='font-bold text-sm uppercase text-gray-200'>
-                      Charlie Howse.
-                    </h6>
-                  </div>
-                </div>
-                <div className='w-full'>
-                  <p className='text-sm leading-tight'>
-                    <span className='text-lg leading-none italic font-bold text-gray-400 mr-1'>
-                      "
-                    </span>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Architecto inventore voluptatum nostrum atque, corrupti,
-                    vitae esse id accusamus dignissimos neque reprehenderit
-                    natus, hic sequi itaque dicta nisi voluptatem! Culpa, iusto.
-                    <span className='text-lg leading-none italic font-bold text-gray-400 ml-1'>
-                      "
-                    </span>
-                  </p>
-                </div>
-              </div>
+              <TestimonialCard testimonials={columnTwo} />
             </div>
-            {/* End of Column 2 */}
-            {/* Column 3 */}
             <div className='px-3 md:w-1/3'>
               <div className='w-full mx-auto rounded-lg TestiCard border border-gray-200 p-5 text-gray-200 font-light mb-6'>
                 <div className='w-full flex mb-4 items-center'>
