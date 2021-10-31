b97618314842ed04549ccaa1172b94f26781c6a0
Jerome Tolentino <tolent.jay@gmail.com>
2021-10-31 16:43:50 +0900

    added column component

--- comps/Testimonials.js
+++ comps/Testimonials.js
@@ -38,6 +38,10 @@ const TestimonialCard = (props) => {
   );
 };
 
+const Column = ({ children }) => {
+  return <div className='px-3 md:w-1/3'>{children}</div>;
+};
+
 const Testimonials = () => {
   const columnOne = [
     {
@@ -61,7 +65,7 @@ const Testimonials = () => {
     {
       id: 3,
       img: 'https://i.pravatar.cc/100?img=3',
-      name: 'Tommie Ewart.',
+      name: 'Tommie Ewart',
       title: 'CEO Workcation',
       quote:
         'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, obcaecati ullam excepturi dicta error deleniti sequi.',
@@ -71,30 +75,60 @@ const Testimonials = () => {
 
   const columnTwo = [
     {
-      id: 3,
+      id: 4,
       img: 'https://i.pravatar.cc/100?img=3',
-      name: 'Tommie Ewart.',
+      name: 'Tommie Ewart',
       title: 'CEO Workcation',
       quote:
         'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, obcaecati ullam excepturi dicta error deleniti sequi.',
       link: '/',
     },
     {
-      id: 4,
+      id: 5,
       img: 'https://i.pravatar.cc/100?img=4',
-      name: 'Charlie Howse.',
+      name: 'Charlie Howse',
       title: 'CEO Workcation',
       quote:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus maxime, magni veritatis maiores vel nisi ab. Voluptatum ipsam, fuga eligendi sint quia ipsa cum corporis numquam nostrum illum facere. Odit.',
       link: '/',
     },
     {
-      id: 5,
+      id: 6,
       img: 'https://i.pravatar.cc/100?img=2',
       name: 'Nevada Herbertson',
       title: 'CEO Workcation',
       quote:
         'Lorem ipsum, dolor sit amet, consectetur adipisicing elit. Dolore quod necessitatibus, labore sapiente, est, dignissimos ullam error ipsam sint quam tempora vel.',
+      link: '/',
+    },
+  ];
+
+  const columnThree = [
+    {
+      id: 7,
+      img: 'https://i.pravatar.cc/100?img=5',
+      name: 'Nevada Herbertson',
+      title: 'CEO Workcation',
+      quote:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, voluptatem porro obcaecati dicta, quibusdam sunt ipsum, laboriosam nostrum facere exercitationem pariatur deserunt tempora molestiae assumenda nesciunt alias eius? Illo, autem!',
+      link: '/',
+    },
+    {
+      id: 8,
+      img: 'https://i.pravatar.cc/100?img=6',
+      name: 'Kris Stanton',
+      title: 'CEO Workcation',
+      quote:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto, explicabo, cupiditate quas totam!',
+      link: '/',
+    },
+    {
+      id: 9,
+      img: 'https://i.pravatar.cc/100?img=1',
+      name: 'Kenzie Edgar',
+      title: 'CEO Workcation',
+      quote:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus maxime, magni veritatis maiores vel nisi ab. Voluptatum ipsam, fuga eligendi sint quia ipsa cum corporis numquam nostrum illum facere. Odit.',
       link: '/',
     },
   ];
@@ -112,69 +146,15 @@ const Testimonials = () => {
             </h3>
           </div>
           <div className='-mx-3 md:flex items-start'>
-            <div className='px-3 md:w-1/3'>
+            <Column>
               <TestimonialCard testimonials={columnOne} />
-            </div>
-            <div className='px-3 md:w-1/3'>
+            </Column>
+            <Column>
               <TestimonialCard testimonials={columnTwo} />
-            </div>
-            <div className='px-3 md:w-1/3'>
-              <div className='w-full mx-auto rounded-lg TestiCard border border-gray-200 p-5 text-gray-200 font-light mb-6'>
-                <div className='w-full flex mb-4 items-center'>
-                  <div className='overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200'>
-                    <img src='https://i.pravatar.cc/100?img=5' alt='' />
-                  </div>
-                  <div className='flex-grow pl-3'>
-                    <h6 className='font-bold text-sm uppercase text-gray-200'>
-                      Nevada Herbertson.
-                    </h6>
-                  </div>
-                </div>
-                <div className='w-full'>
-                  <p className='text-sm leading-tight'>
-                    <span className='text-lg leading-none italic font-bold text-gray-400 mr-1'>
-                      "
-                    </span>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nobis, voluptatem porro obcaecati dicta, quibusdam sunt
-                    ipsum, laboriosam nostrum facere exercitationem pariatur
-                    deserunt tempora molestiae assumenda nesciunt alias eius?
-                    Illo, autem!
-                    <span className='text-lg leading-none italic font-bold text-gray-400 ml-1'>
-                      "
-                    </span>
-                  </p>
-                </div>
-              </div>
-              <div className='w-full mx-auto rounded-lg TestiCard border border-gray-200 p-5 text-gray-200 font-light mb-6'>
-                <div className='w-full flex mb-4 items-center'>
-                  <div className='overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200'>
-                    <img src='https://i.pravatar.cc/100?img=6' alt='' />
-                  </div>
-                  <div className='flex-grow pl-3'>
-                    <h6 className='font-bold text-sm uppercase text-gray-200'>
-                      Kris Stanton.
-                    </h6>
-                    <backqoutes class='font-normal text-white text-sm tracking-wider'>
-                      CEO Workcation
-                    </backqoutes>
-                  </div>
-                </div>
-                <div className='w-full'>
-                  <p className='text-sm leading-tight'>
-                    <span className='text-lg leading-none italic font-bold text-gray-400 mr-1'>
-                      "
-                    </span>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Voluptatem iusto, explicabo, cupiditate quas totam!
-                    <span className='text-lg leading-none italic font-bold text-gray-400 ml-1'>
-                      "
-                    </span>
-                  </p>
-                </div>
-              </div>
-            </div>
-            {/* End of Column 3 */}
+            </Column>
+            <Column>
+              <TestimonialCard testimonials={columnThree} />
+            </Column>
           </div>
         </div>
       </div>
