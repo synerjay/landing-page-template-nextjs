c528210f26222519797b95be7914d24453bfbfe7
Jerome Tolentino <tolent.jay@gmail.com>
2021-11-01 19:49:26 +0900

    added conditions on action card

--- comps/Action.js
+++ comps/Action.js
@@ -24,6 +24,53 @@ const Action = () => {
             Get a Demo
           </a>
         </div>
+        <div className='flex justify-center items-center gap-x-10'>
+          <span className='flex items-center gap-x-1'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-5 w-5'
+              viewBox='0 0 20 20'
+              fill='currentColor'
+            >
+              <path
+                fillRule='evenodd'
+                d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
+                clipRule='evenodd'
+              />
+            </svg>{' '}
+            30-day free trial{' '}
+          </span>
+          <span className='flex items-center gap-x-1'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-5 w-5'
+              viewBox='0 0 20 20'
+              fill='currentColor'
+            >
+              <path
+                fillRule='evenodd'
+                d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
+                clipRule='evenodd'
+              />
+            </svg>{' '}
+            No credit card required{' '}
+          </span>
+          <span className='flex items-center gap-x-1'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-5 w-5'
+              viewBox='0 0 20 20'
+              fill='currentColor'
+            >
+              <path
+                fillRule='evenodd'
+                d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
+                clipRule='evenodd'
+              />
+            </svg>{' '}
+            Cancel anytime{' '}
+          </span>
+        </div>
       </div>
     </div>
   );
