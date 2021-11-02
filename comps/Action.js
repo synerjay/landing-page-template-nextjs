d71faf8cb47a2251fb3287e409b6b71b689fe525
Jerome Tolentino <tolent.jay@gmail.com>
2021-11-02 18:53:53 +0900

    changed the action component

--- comps/Action.js
+++ comps/Action.js
@@ -4,10 +4,10 @@ const Action = () => {
   return (
     <div className='Testimonials py-2 flex flex-col justify-center mx-auto sm:py-12'>
       <div className='h-auto py-10 px-10 w-2/3 bg-indigo-500 flex flex-col justify-center items-center space-y-7 mx-auto rounded-3xl shadow-xl hover:rotate-1 transition-transform'>
-        <h1 className='text-white font-bold text-4xl tracking-wide'>
+        <h1 className='text-white font-bold text-4xl tracking-tight'>
           Ready to take the plunge?
         </h1>
-        <h2 className='font-normal tracking-wide text-md text-white'>
+        <h2 className='font-normal tracking-tight text-lg text-white'>
           Here's some room for that final call to action
         </h2>
         <div className='flex justify-center items-center'>
