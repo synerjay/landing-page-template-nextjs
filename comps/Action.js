180a8c62fce55aef6f6fd51f4649e4f622431bf4
Jerome Tolentino <tolent.jay@gmail.com>
2021-11-06 23:27:46 +0900

    added action component

--- comps/Action.js
+++ comps/Action.js
@@ -3,14 +3,14 @@ import React from 'react';
 const Action = () => {
   return (
     <div className='Testimonials py-2 flex px-5 flex-col justify-center items-center mx-auto sm:py-12'>
-      <div className='h-auto py-7 md:py-10 px-7 md:px-10 w-full md:w-2/3 bg-indigo-500 flex flex-col justify-center items-center space-y-7 md:mx-auto rounded-3xl shadow-xl hover:rotate-1 transition-transform'>
+      <div className='h-auto py-7 md:py-10 px-7 md:px-10 w-full md:w-2/3 bg-indigo-500 flex flex-col justify-center items-center text-center space-y-7 md:mx-auto rounded-3xl shadow-xl hover:rotate-1 transition-transform'>
         <h1 className='text-white font-bold text-4xl tracking-tight'>
           Ready to take the plunge?
         </h1>
         <h2 className='font-normal tracking-tight text-lg text-white'>
           Here's some room for that final call to action
         </h2>
-        <div className='flex justify-center items-center'>
+        <div className='flex flex-col space-y-5 md: space-y-0 md:flex-row justify-center items-center'>
           <a
             href=''
             className='bg-gray-900 rounded-full font-bold text-white px-4 py-3 transition duration-300 ease-in-out hover:bg-gray-800 mr-6'
