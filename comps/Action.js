91802be24d9f493fb3736f276a7db9eb56b28ee0
Jerome Tolentino <tolent.jay@gmail.com>
2021-11-08 11:35:47 +0900

    Changed the action components

--- comps/Action.js
+++ comps/Action.js
@@ -4,7 +4,7 @@ const Action = () => {
   return (
     <div className='Testimonials py-2 flex px-5 flex-col justify-center items-center mx-auto sm:py-12'>
       <div className='h-auto py-7 md:py-10 px-7 md:px-10 w-full md:w-2/3 bg-indigo-500 flex flex-col justify-center items-center text-center space-y-3 md:space-y-7 md:mx-auto rounded-3xl shadow-xl hover:rotate-1 transition-transform'>
-        <h1 className='text-white font-bold text-2xl md:text-4xl tracking-tight'>
+        <h1 className='text-white font-bold text-2xl md:text-5xl tracking-tight'>
           Ready to take the plunge?
         </h1>
         <h2 className='font-normal tracking-tight text-md md:text-lg text-white'>
