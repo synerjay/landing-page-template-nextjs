4b9e61b341cad2fad468b76f82d0292b20cb19dd
Jerome Tolentino <tolent.jay@gmail.com>
2021-11-12 19:49:33 +0900

    Changed 404 page

--- pages/404.js
+++ pages/404.js
@@ -5,22 +5,24 @@ import { useRouter } from 'next/router';
 const NotFound = () => {
   const router = useRouter(); // routes the use to another page
 
-  useEffect(() => {
-    setTimeout(() => {
-      router.push('/');
-    }, 3000);
-  }, []);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     router.push('/');
+  //   }, 3000);
+  // }, []);
 
   return (
-    <div className='not-found'>
-      <h1>Oooooops....</h1>
-      <h2>That page cannot be found</h2>
-      <p>
-        Go back to the{' '}
-        <Link href='/'>
-          <a>Homepage</a>
-        </Link>{' '}
-      </p>
+    <div className='h-screen w-screen flex flex-col justify-center items-center'>
+      <div className='flex flex-col justify-center items-center -mt-24'>
+        <h1 className='text-3xl'>Oops...</h1>
+        <h2>That page cannot be found</h2>
+        <p>
+          Go back to the{' '}
+          <Link href='/'>
+            <a>Homepage</a>
+          </Link>{' '}
+        </p>
+      </div>
     </div>
   );
 };
